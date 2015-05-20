6cfe52b677d0ab2513fb9ffe2819a342eb43b65c
Pillet Thomas <thomas.pillet@etu.univ-orleans.fr>
2015-05-20 15:42:48 +0200

    Edit : move and stop when an obstacle is at 30 cm

--- example/utrasonicSensor/utrasonicSensor.ino
+++ example/utrasonicSensor/utrasonicSensor.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ArduinoRobot.h>
 
+#include "USSEnsorUtils.h";
+#include <Wire.h>
+#include <SPI.h>
+
+// pin is used by the sensors
+int rightPin = M0;
+int frontRightPin = M1;
+int frontPin = M2;
+int frontLeftPin = M3;
+int leftPin = M4;
+int backleftPin = M5;
+int backPin = M6;
+int backRightPin = M7;
+
+
+void setup() {
+  Serial.begin(9600);
+  Robot.begin();
+  Robot.beginTFT();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(getDistance(frontPin)<30){
+    Robot.motorsStop();
+  }else{
+    Robot.motorsWrite(170,170);
+   }
 }
