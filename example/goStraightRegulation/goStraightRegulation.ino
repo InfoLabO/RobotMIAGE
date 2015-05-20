4168e7573403699d2bf3a54e7bc3c62692578861
amine-stage <amine3100.boufatah@hotmail.com>
2015-05-20 15:28:48 +0200

    Testing going forward and turning right or left

--- example/goStraightRegulation/goStraightRegulation.ino
+++ example/goStraightRegulation/goStraightRegulation.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
 
-}
+#include <ArduinoRobot.h>
+#include <SPI.h>
+#include <Wire.h>
+
+void setup() {
+  Robot.begin();
+  Robot.beginTFT();
+  Robot.beginSD();
+  Robot.displayLogos();
+} 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  goStraight();
+  stopIt();
+  delay(1000);
+  turnRight();
+  delay(1000);
+  goStraight();
+  turnLeft();
+  delay(1000);
+}
 
+/**
+Turning left 
+**/
+void turnLeft(){
+  Robot.motorsWrite(0 , 125 ) ; 
+  
+}
+
+/**
+Turning Right 
+**/
+void turnRight(){
+  Robot.motorsWrite(125 , 0 ) ; 
+}
+
+/**
+Stopping the robot 
+**/
+void stopIt() {   
+  Robot.stop() ; 
+}
+
+/**
+Going straight
+**/
+void goStraight(){
+  Robot.motorsWrite(255 , 255 ) ; 
+  delay(1000);
 }
