78d4bb9a7c996e70e6a1a480d3eb22fc4895d306
Pillet Thomas <thomas.pillet@etu.univ-orleans.fr>
2015-05-26 17:23:15 +0200

    Revert "added functions PID to orientationLibrary.h"
    This reverts commit 8351a2d3bf97ceb03bf98b5f1f89bfbca6f8cf74.

--- example/orientationRegulation/orientationLibrary.h
+++ example/orientationRegulation/orientationLibrary.h
@@ -3,5 +3,3 @@
 
 void turn(int angle);
 void pointToCustom(int angle, int speed);
-int errAngular(int angleActuel,int angleObjectif);
-void pidAngular(int erreur, int erreurPrecedente, int &ML,int &MR int &sommeErreur);
