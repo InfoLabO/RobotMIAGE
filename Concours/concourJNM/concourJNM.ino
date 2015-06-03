d2d3ef468dc973f7d477eebc1ab7338c0e5e8c63
Pillet Thomas <thomas.pillet@etu.univ-orleans.fr>
2015-06-03 09:58:38 +0200

    added includes

--- Concours/concourJNM/concourJNM.ino
+++ Concours/concourJNM/concourJNM.ino
@@ -1,3 +1,12 @@
+#include <ArduinoRobot.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include "USSensorUtils.h"
+#include "goStraightPID.h"
+#include "orientationLibrary.h"
+#include "turn.h"
+
 void setup() {
   // put your setup code here, to run once:
 
